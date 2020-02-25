548630184a74ad484edc0ffd6fce9bebf9171b06
Hubert Yang <offspring7890@yahoo.com>
2020-02-24 20:18:47 -0800

    almost working solution for #138

--- LeetCode/138.CopyListWithRandomPointer.js
+++ LeetCode/138.CopyListWithRandomPointer.js
@@ -11,5 +11,33 @@
  * @return {Node}
  */
 var copyRandomList = function(head) {
-    
+  let copy = new Node(NaN, null, null);
+  let newCopy = copy;
+  let current = head;
+  let map = new Map();
+
+  //iterate through node list until there's no head
+  while(current) {
+    //create cloned node with value
+    let newNode = new Node(current.val, null, null)
+    newCopy = newNode;
+    //copies value of current node and newNode in a [key,value] map pair
+    map.set(current, newNode)
+
+    //iterates through the copy and original nodes next pointer
+    newCopy = newCopy.next;
+    current = current.next;
+  }
+
+  newCopy = copy.next;
+  current = head;
+
+  while(current) {
+    if(current.random) {
+      newCopy.random = map.get(current.random)
+    }
+    newCopy = newCopy.next;
+    current = current.next;
+  }
+  return copy.next;
 };
