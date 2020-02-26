83fee8baf50c4f6b3da3ac9f50c0b0d5f61634ba
Hubert Yang <offspring7890@yahoo.com>
2020-02-25 21:07:09 -0800

    solved #819

---
+++ LeetCode/819.MostCommonWord.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} paragraph
+ * @param {string[]} banned
+ * @return {string}
+ * https://leetcode.com/problems/most-common-word/
+ */
+var mostCommonWord = function(paragraph, banned) {
+    let obj = {}, res = '', max = -1;
+    let words = paragraph.split(/[\!\?\'\,\;\.\s]/)
+    console.log(words)
+
+    words.forEach(w => {
+      let word = w.toLowerCase()
+      if(!banned.includes(word)) {
+        obj[word] = obj[word] == undefined ? 1 : obj[word]+1 
+      }
+    })
+
+    for(let key in obj) {
+      if(obj[key] > max) {
+        //console.log(obj[key])
+        max = obj[key]
+        res = key
+      }
+    }
+    return res;
+};
+mostCommonWord('Hello, my. friend. i am, hello', 'i')
